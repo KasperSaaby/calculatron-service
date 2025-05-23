c4ae82de86692cd6411583bdf150f4b4b934739c
Kasper Saaby <ksa@lunar.app>
2025-05-23 12:15:19 +0200

    Adds gen file for swagger

---
+++ gen_swagger.go
@@ -0,0 +1,4 @@
+package calculatron_service
+
+// install go-swagger: https://goswagger.io/go-swagger/install/install-binary/
+//go:generate /opt/homebrew/bin/swagger generate server --spec api/swagger.yaml --name calculatron-service --exclude-main --target ./generated
