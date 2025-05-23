7c189a7cf563d300293a3538ef1333184c6aa5a1
Kasper Saaby <ksa@lunar.app>
2025-05-23 11:10:58 +0200

    Move test of decorator

--- internal/app/calculator_service_test.go
+++ internal/app/calculator_service_test.go
@@ -6,48 +6,8 @@ import (
 
 	"github.com/KasperSaaby/calculatron-service/internal/domain/operations"
 	"github.com/KasperSaaby/calculatron-service/internal/domain/values"
-	"github.com/KasperSaaby/calculatron-service/internal/store"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
-
-func Test_CalculatorServiceDecorator_PerformCalculation(t *testing.T) {
-	operationFactory := operations.NewOperationFactory()
-	storeFactory, err := store.GetStoreFactory(store.InMemory_Type, nil)
-	require.NoError(t, err)
-
-	t.Run("successful addition and save to history", func(t *testing.T) {
-		ctx := context.Background()
-		historyStore, err := storeFactory.CreateHistoryStore()
-		require.NoError(t, err)
-		calculatorService := NewCalculatorService(operationFactory)
-		sut := NewCalculatorServiceDecorator(calculatorService, historyStore)
-
-		operationType := values.OperationType_Add
-		operands := []float64{1, 2}
-		precision := 2
-		expectedResult := 3.00
-
-		result, err := sut.PerformCalculation(ctx, operationType, operands, precision)
-
-		// Assert that calculation was performed successfully
-		assert.NoError(t, err)
-		assert.Equal(t, expectedResult, result.Result)
-		assert.Equal(t, precision, result.Precision)
-		assert.NotEmpty(t, result.OperationID)
-		assert.NotZero(t, result.Timestamp)
-
-		// Assert that calculation was stored in history
-		history, err := historyStore.GetAllCalculations(ctx, 0, 1)
-		require.NoError(t, err)
-		require.Len(t, history, 1)
-		assert.Equal(t, result.OperationID, history[0].OperationID)
-		assert.Equal(t, operationType, history[0].OperationType)
-		assert.Equal(t, operands, history[0].Operands)
-		assert.Equal(t, expectedResult, history[0].Result)
-		assert.Equal(t, precision, history[0].Precision)
-	})
-}
 
 func Test_CalculatorService_PerformCalculation(t *testing.T) {
 	testCases := []struct {
